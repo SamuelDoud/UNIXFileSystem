2eb4dee70e209b4026cbc37cf5f1279836ef09c1
SamuelDoud <sd8396a@student.american.edu>
2015-11-19 21:43:13 -0500

    Created a struct for index file table

---
+++ FileTable.h
@@ -0,0 +1,10 @@
+#ifndef __File_Table_H__
+#define __File_Table_H__
+
+typedef struct File_Table_Element {
+  int inodePointer;
+  int index;
+  int fileOpenCount
+} File_Table_Element;
+
+#endif // __Disk_H__
