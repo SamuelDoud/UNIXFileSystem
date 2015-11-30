28d1e3eb920210687249d035920091b7b515a8d9
samueldoud <sd8396a@student.american.edu>
2015-11-30 11:15:16 -0500

    More by reference

--- Map.h
+++ Map.h
@@ -1,6 +1,7 @@
 #ifndef MAP_H_INCLUDED
 #define MAP_H_INCLUDED
 
+#include "LibDisk.h"
 //map contains the data to track a bitmap and a bytemap! Should have two in the file system. One for the data and one for the inode bitmaps!
 typedef struct Map{
     int lengthBitmap;//integer indicating how long the bitmap array is. Could be a short or something
@@ -17,9 +18,10 @@ bool ChangeBitmap(Map *mapArg, int sectorNum, bool TrueOrFalse)
         mapArg.bitmap[sectorNum] = TrueOrFalse;
         mapArg = ConvertBitmapToBytemap(mapArg);//make sure the bytemap is reflective!
     }
+    return true;
 }
 //THIS METHOD IS NOT COMPLETE
-Map ConvertBitmapToBytemap(Map mapData)
+Map ConvertBitmapToBytemap(Map *mapData)
 {
     bool *bitmap = mapData.bitmap;
     int lengthOfBitmap = mapData.lengthBitmap;
@@ -43,8 +45,10 @@ Map ConvertBitmapToBytemap(Map mapData)
     }
     //take every eight bits and convert them into a character
     //stitch together all results to make the bytemap
-
-    return mapData;
+    return true;
 }
-
+bool UpdateBytemapOnDisk(Sector *someDisk, int startSector, int numOfSectors)
+{
+    //TODO (Sam#2#):
+}
 #endif // MAP_H_INCLUDED
