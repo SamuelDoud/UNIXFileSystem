ec23476608d42a094043379f1524f5e60ae65e53
samueldoud <sd8396a@student.american.edu>
2015-11-30 11:24:48 -0500

    I'm not modifying structs pointers correctly

--- FileTable.h
+++ FileTable.h
@@ -4,7 +4,13 @@
 typedef struct FileTableElement {
   int inodePointer;
   int index;
-  int fileOpenCount
+  int fileOpenCount;
+  int sizeOfFile;
 } FileTableElement;
+//should be all zeros initily
 
+bool SetToNull(FileTableElement *entry)
+{
+    entry = NULL;
+}
 #endif // __Disk_H__
